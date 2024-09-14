184c5f487b88b49282f567cae0934a07b9989e12
devCortes <dcortes@dantecortes.dev>
2024-09-14 09:37:42 +0000

    Flexible health header and blurs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from 'react';
-import Header from './components/Header';
+import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import BlurMobileSup from './assets/BlurMobileSup.png';
+import BlurMobileInf from './assets/BlurMobileInf.png';
+import BlurDesktopRight from './assets/BlurDesktopRight.png';
+import BlurDesktopLeft from './assets/BlurDesktopLeft.png';
 import './styles/app.scss';
 
 function App() {
-	const [user, setUser] = useState('');
-
-	useEffect(() => {
-		
-	}, []);
-
 	return (
 		<>
+			<img className='blur-mobile-sup' src={BlurMobileSup} alt="" />
+			<img className='blur-mobile-inf' src={BlurMobileInf} alt="" />
+			<img className='blur-desktop-right' src={BlurDesktopRight} alt="" />
+			<img className='blur-desktop-left' src={BlurDesktopLeft} alt="" />
 			<Header></Header>
 			<Footer />
 		</>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
-function Header(){
+import LogoHeader from '../assets/LogoHeader.png';
+import PhoneIcon from '../assets/PhoneIcon.png';
+
+export function Header(){
     return (
-        <>
-            <header>
-                Header
-            </header>
-        </>
+        <header>
+            <img src={LogoHeader} alt="Logo Header" />
+            <div className="phone-number">
+                <p className='buy-notice'>¡Compra por este medio!</p>
+                <img src={PhoneIcon} alt="Phone Icon" />
+                <p className='phone-text'>(01) 411 6001</p>
+            </div>
+        </header>
     )
 }
-
-export default Header;
